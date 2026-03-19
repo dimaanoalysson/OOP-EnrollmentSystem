3f1c34eb9238358ddf14006b5a8fafb1f1182992
Alysson Ninette Dimaano <dimaanoalysson@gmail.com>
2026-03-20 07:24:10 +0800

    Tuition Fee

--- src/main/java/org/example/model/Student.java
+++ src/main/java/org/example/model/Student.java
@@ -2,16 +2,19 @@ package org.example.model;
 
 public class Student extends Person {
     private String program;
+    private TuitionFeePayment paymentTransaction;
 
     //no-args constructor
     public Student(){
         super();
+        this.paymentTransaction = new TuitionFeePayment();
     }
 
     // Constructor
     public Student(String id, String name, String program){
         super(id, name);
         this.program = program;
+        this.paymentTransaction = new TuitionFeePayment();
     }
 
     public void setProgram(String program){
@@ -20,5 +23,9 @@ public class Student extends Person {
 
     public String getProgram(){
         return program;
+    }
+
+    public TuitionFeePayment getPaymentTransaction() {
+        return paymentTransaction;
     }
 }
